43d62e9705f785f03caebe4503cd520a2dcc5f3b
Peter <ppapadim12@gmail.com>
2017-06-08 16:37:12 -0400

    created school class which creates/adds/sorts students and their grade into a roster.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,32 @@
-# code here!
+require "Pry"
+
+class School
+
+	def initialize(name)
+	  @name = name
+	  @roster = {}
+	end
+
+	def roster
+	  @roster
+	end
+
+	def add_student(student_name, student_grade)
+	  if @roster.has_key?(student_grade)
+		  @roster[student_grade] << student_name
+	  else 
+		  @roster[student_grade] = [student_name]
+	  end
+	end
+
+	def grade(student_grade)
+	  @roster[student_grade]
+	end
+
+	def sort
+		@roster.each do |student_grade, student_list|
+		  student_list.sort!
+		end
+	end
+
+end
